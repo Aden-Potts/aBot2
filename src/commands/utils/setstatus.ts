1bbbf206cd0d31be0109048cdda0da6b2a335499
Aden Potts <adenpotts01@icloud.com>
2023-02-14 20:13:15 -0500

    Update setstatus.ts, delete test.ts

--- src/commands/utils/setstatus.ts
+++ src/commands/utils/setstatus.ts
@@ -13,8 +13,13 @@ module.exports = {
         {type:"str", name:"status", desc:"some status", req:true}
     ],
     exec(interaction: ChatInputCommandInteraction, abot: Bot) {
+        if(interaction.member?.user.id != "195644811821318145") {
+            interaction.reply({content: "Only Adamantite can use this command, nerd!", ephemeral:true});
+        }
         let text: string = interaction.options.getString("status") ?? "";
 
         abot.clientObj.user?.setActivity(text);
+
+        interaction.reply({content:"Status has been set.", ephemeral:true});
     }
 }
